d977aa19de958919bd782ceef266f4bd09d1299a
Connor Lane Smith <cls@lubutu.com>
2017-02-17 23:55:30 +0000

    Add invariants on vector and matrix multiplication

--- Matrix.hs
+++ Matrix.hs
@@ -30,11 +30,26 @@ mapElems f = Matrix . map (map f) . rows
 (<:>) :: Semiring a => Row a -> Column a -> a
 Row v <:> Column w = foldr (<+>) zero $ zipWith (<.>) v w
 
+prop_singleInner :: (Eq a, Semiring a) => a -> a -> Bool
+prop_singleInner x y = (Row [x] <:> Column [y]) == (x <.> y)
+
 (<\>) :: Semiring a => Row a -> Matrix a -> Row a
 v <\> m = mapCols (v <:>) m
+
+prop_singleRowMul :: (Eq a, Semiring a) => Row a -> Column a -> Bool
+prop_singleRowMul v w@(Column xs) = let Row [z] = v <\> Matrix (map return xs)
+                                    in z == (v <:> w)
 
 (</>) :: Semiring a => Matrix a -> Column a -> Column a
 m </> v = mapRows (<:> v) m
 
+prop_singleColumnMul :: (Eq a, Semiring a) => Row a -> Column a -> Bool
+prop_singleColumnMul v@(Row xs) w = let Column [z] = Matrix (return xs) </> w
+                                    in z == (v <:> w)
+
 instance Semiring a => Semigroup (Matrix a) where
   m <> n = coerce $ mapRows (<\> n) m
+
+prop_singleMatrixMul :: (Eq a, Semiring a) => Row a -> Column a -> Bool
+prop_singleMatrixMul v@(Row xs) w@(Column ys) = let Matrix [[z]] = Matrix (return xs) <> Matrix (map return ys)
+                                                in z == (v <:> w)
