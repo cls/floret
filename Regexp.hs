83a718bb8aafb99ab612dcaafaf6131f59da8189
Connor Lane Smith <cls@lubutu.com>
2017-02-22 20:48:18 +0000

    Complete Glushkov construction with start position

--- Regexp.hs
+++ Regexp.hs
@@ -12,6 +12,13 @@ data Regexp c a = Weight a
                 | Concat (Regexp c a) (Regexp c a)
                 | Kleene (Regexp c a)
 
+letters :: Regexp c a -> [c -> a]
+letters (Weight _)   = []
+letters (Letter f)   = [f]
+letters (Choice r s) = letters r ++ letters s
+letters (Concat r s) = letters r ++ letters s
+letters (Kleene s)   = letters s
+
 -- Functions for defining extended Glushkov automata.
 
 null :: StarSemiring a => Regexp c a -> a
@@ -35,14 +42,27 @@ last (Choice r s) = last r ++ last s
 last (Concat r s) = map (<.> null s) (last r) ++ last s
 last r@(Kleene s) = map (<.> null r) (last s)
 
-follow :: StarSemiring a => Regexp c a -> c -> Matrix a
-follow (Weight _)   _ = []
-follow (Letter f)   c = [[f c]]
-follow (Choice r s) c = let r' = follow r c
-                            s' = follow s c
-                        in blockAD r' s'
-follow (Concat r s) c = let r' = follow r c
-                            s' = follow s c
-                        in blockABD r' (last r >< first s <> s') s'
-follow (Kleene s)   c = let s' = follow s c
-                        in zipElemsWith (<+>) s' (last s >< first s <> s')
+follow :: StarSemiring a => Regexp c a -> Matrix a
+follow (Weight _)   = []
+follow (Letter f)   = [[unit]]
+follow (Choice r s) = let r' = follow r
+                          s' = follow s
+                      in blockAD r' s'
+follow (Concat r s) = let r' = follow r
+                          s' = follow s
+                      in blockABD r' (last r >< first s <> s') s'
+follow (Kleene s)   = let s' = follow s
+                      in zipElemsWith (<+>) s' (last s >< first s <> s')
+
+-- Glushkov automata as matrices over a semiring.
+
+initial :: StarSemiring a => Regexp c a -> Row a
+initial r = unit : map (const zero) (letters r)
+
+final :: StarSemiring a => Regexp c a -> Column a
+final r = null r : last r
+
+delta :: StarSemiring a => Regexp c a -> c -> Matrix a
+delta r c = map delta' (first r : follow r)
+  where
+    delta' = (zero :) . zipWith ((<.>) . ($ c)) (letters r)
