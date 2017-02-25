d193015addc852191979131e8480ec46e0316c02
Connor Lane Smith <cls@lubutu.com>
2017-02-25 17:17:58 +0000

    Add some testable properties for matrices

--- Test/Matrix.hs
+++ Test/Matrix.hs
@@ -17,6 +17,13 @@ prop_singleColumnMul :: (Eq a, Semiring a) => Row a -> Column a -> Bool
 prop_singleColumnMul v w = let [z] = row v </> w
                            in z == v <:> w
 
-prop_singleMatrixMul :: (Eq a, Semiring a) => Row a -> Column a -> Bool
-prop_singleMatrixMul v w = let [[z]] = row v <> column w
-                           in z == v <:> w
+prop_matrixInner :: (Eq a, Semiring a) => Row a -> Column a -> Bool
+prop_matrixInner v w = case row v <.> column w of
+                         [[z]] -> z == v <:> w
+                         [[ ]] -> null v || null w
+
+prop_matrixOuter :: (Eq a, Semiring a) => Column a -> Row a -> Bool
+prop_matrixOuter v w = v >< w == column v <.> row w
+
+prop_matrixAddId :: (Eq a, Semiring a) => Matrix a -> Bool
+prop_matrixAddId m = m <+> mapElems (const zero) m == m
