54b347c86b67d970acbc9b4e52aaafb9ba1e3aac
Connor Lane Smith <cls@lubutu.com>
2017-02-25 17:00:18 +0000

    Replace `letters' with higher-order `weigh' function

--- Regexp.hs
+++ Regexp.hs
@@ -12,12 +12,12 @@ data Regexp c a = Weight a
                 | Concat (Regexp c a) (Regexp c a)
                 | Kleene (Regexp c a)
 
-letters :: Regexp c a -> [c]
-letters (Weight _)   = []
-letters (Letter x)   = [x]
-letters (Choice r s) = letters r ++ letters s
-letters (Concat r s) = letters r ++ letters s
-letters (Kleene s)   = letters s
+weigh :: Semiring a => (c -> a) -> Regexp c a -> [a]
+weigh f (Weight _)   = []
+weigh f (Letter x)   = [f x]
+weigh f (Choice r s) = weigh f r ++ weigh f s
+weigh f (Concat r s) = weigh f r ++ weigh f s
+weigh f (Kleene s)   = weigh f s
 
 -- Functions for defining extended Glushkov automata.
 
@@ -57,13 +57,11 @@ follow (Kleene s)   = let s' = follow s
 -- Glushkov automata as matrices over a semiring.
 
 initial :: StarSemiring a => Regexp c a -> Row a
-initial r = unit : map (const zero) (letters r)
+initial r = unit : weigh (const zero) r
 
 final :: StarSemiring a => Regexp c a -> Column a
 final r = null r : last r
 
-delta :: (Eq c, StarSemiring a) => Regexp c a -> c -> Matrix a
+delta :: StarSemiring a => Regexp c a -> (c -> a) -> Matrix a
 delta r = let m = first r : follow r
-              xs = letters r
-          in \y -> let weigh = zipWith (<.>) (map (fromBool . (== y)) xs)
-                   in mapRows ((zero :) . weigh) m
+          in \f -> mapRows (\v -> zero : zipWith (<.>) (weigh f r) v) m
